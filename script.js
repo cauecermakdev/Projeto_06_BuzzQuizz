0bcf37d507380ca4e21e3d3f068d9c2e9a7f1202
Cauê Pereira Cermak <cauecermak@gmail.com>
2022-09-02 21:23:31 +0000

    mix tela 1,2 e 3

--- script.js
+++ script.js
@@ -1,4 +1,83 @@
 
+//*****************************************************
+// SCRIPT LAYOUT 1 ************************************
+  function deuruim(error){
+    console.log("entra");
+    console.log(error);
+ }
+
+
+function iniciaTela() {
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+
+    promessa.then(comecaQuizz);
+    promessa.catch(deuruim); 
+}
+iniciaTela(); 
+
+function comecaQuizz(resposta) {
+    console.log("aqui");
+    console.log(resposta);
+    const url = resposta.data[0].image;
+
+    const printaTela = document.querySelector(".tela1");
+    printaTela.innerHTML +=
+        `
+            <div class="container display-flex-center">
+                <div class="conteudo-pagina display-flex-center">
+
+                    <div class="seus-quizzes-vazio display-flex-center">
+                        <h1>Você não criou nenhum quizz ainda :(</h1>
+                        <div class="botao display-flex-center">Criar Quizz</div>
+
+                    </div>
+                    <div class="seus-quizzes-conteudo display-none">
+                        <div class=" subtitulo display-flex-center">Seus Quizzes <img src="./img/botaoadd.png"></div>
+                        <div class="quizz"><img src="./img/Potterhead.png"><a class="titulo-quizz">O quão Potterhead é
+                            você?
+                            O quão Potterhead é você? </a></div>
+                        <div class="quizz"><img src="./img/Potterhead.png"><a class="titulo-quizz">O quão Potterhead é
+                            você?</a></div>
+                        <div class="quizz"><img src="./img/Potterhead.png"><a class="titulo-quizz">O quão Potterhead é
+                            você?</a></div>
+                    </div>
+
+                    <div class="todos-os-quizzes display-flex-center">
+                        <div class="subtitulo">Todos os Quizzes</div>
+
+                    </div>
+
+                </div>
+            </div>
+        `
+    const puxaQuizz = document.querySelector(".todos-os-quizzes");
+    for (let i = 0; i < resposta.data.length; i++) {
+        puxaQuizz.innerHTML += `<div class="quizz" onclick=""><img src="${resposta.data[i].image}">
+                                <a class="titulo-quizz">${resposta.data[i].title}</a></div>`
+    }
+
+
+}
+
+
+
+
+
+//*****************************************************
+// SCRIPT LAYOUT 3 ************************************
+
+
+//variaveis do quiz que vai ser criado
+//pegar dados da tela anterior antes de colocar a segunda tela
+const titulo = "";
+const url_img_quizz = "";
+const n_perguntas = 0;
+const n_niveis= 0 ;
+
+
+const  array_answers = [];
+
+
 //General functions**************************
 
 function scroll_to(string){
@@ -23,7 +102,7 @@ function setTelaUmCriaQuiz() {
             <input placeholder ="Título do seu quizz" type="text" minlength="20" maxlength="65" id="titulo-quizz" required/>
             <input placeholder ="URL da imagem do seu quizz" type="url" name="userURL" id="url-img-quizz" required/>
             <input placeholder ="Quantidade de perguntas do quizz" type="number" name="quantity" min="3" id="n-perguntas-quizz" required/>
-            <input placeholder ="Quantidade de níveis do quizz" type="number" name="quantity" min="2" id="n-niveis" required/>
+            <input placeholder ="Quantidade de níveis do quizz" type="number" name="quantity" min="2" id="n-niveis-quizz" required/>
         </div>  
 
     <button type="submit" class="btn-primario">Prosseguir para criar perguntas</button>
@@ -31,77 +110,99 @@ function setTelaUmCriaQuiz() {
     `
 }
 
-setTelaUmCriaQuiz();//essa funcao chama o Layout 3
+//setTelaUmCriaQuiz();//essa funcao chama o Layout 3
 
 
+function colocaPergunta(){
+    return `
+    <div class="card-inputs-quiz"  data-identifier="question-form">
+        <div class="group-inputs">
+            <div class="card-edit select">
+                <p class="title">Pergunta 1</p>
+                <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
+            </div>
+                <input placeholder="Texto da pergunta" type="text" minlength="20" required/>
+                <input placeholder="Cor de fundo da pergunta" type="text" maxlength="7" minlength="7">
+        </div>
+
+        <!--<div class="pergunta-content">-->
+        <!--talvez faça sentido colocar pra esconder dps-->
+        <div class="group-inputs">
+            <p class="title">Resposta correta</p>
+            <input placeholder="Resposta correta" type="text" required/>
+            <input placeholder="URL da imagem" type="url">
+        </div>
+
+        <div class="group-inputs">
+            <p class="title">Respostas incorretas</p>
+            <input placeholder="Resposta incorreta 1" type="text" required/>
+            <input placeholder="URL da imagem 1" type="url">
+        </div>
+            
+        <div class="group-inputs">
+            <input placeholder="Resposta incorreta 2" type="text"> 
+            <input placeholder="URL da imagem 2" type="url">
+        </div>
+
+        <div class="group-inputs">
+            <input placeholder="Resposta incorreta 3" type="text">
+            <input placeholder="URL da imagem 3" type="url">
+        </div>
+        <!-- fim de <div class="pergunta-content">-->
+
+        <!--fim do group-inputs-->
+    </div>
+`;
+
+    console.log("entrou");
+}
 
 //tela 3.2
 function setTelaDoisCriaQuiz() {
-/* 
-    if(!trata_inputs_tela1_cria_quizz()){
-        return;//if(function())return false nao roda
-    } */
+    
+    alert("aqui");
+    //pegar dados da tela anterior antes de colocar a segunda tela
+    titulo = document.querySelector("#titulo-quizz").value;
+    console.log(titulo);
+    url_img_quizz = document.querySelector("#url-img-quizz").value;
+    console.log(url_img_quizz);
+    n_perguntas = parseInt(document.querySelector("#n-perguntas-quizz").value);
+    console.log(n_perguntas);
+    n_niveis = parseInt(document.querySelector("#n-niveis-quizz").value);
+    console.log(n_niveis);
 
-    //troca corpo
+    //comeco da tela 2
     document.querySelector(".cria-quiz").innerHTML = `
-    <p class="title">Crie suas perguntas</p>
+        <p class="title">Crie suas perguntas</p>
+        <form onSubmit = "setTelaTresCriaQuiz()">
+    `;
 
-    <form onSubmit = "setTelaTresCriaQuiz()">
+    for(let i = 0; i < n_perguntas; i++ ){
+    //troca corpo
+        document.querySelector(".cria-quiz").innerHTML += `
 
-        <div class="card-inputs-quiz"  data-identifier="question-form">
-            <div class="group-inputs">
-                <div class="card-edit select">
-                    <p class="title">Pergunta 1</p>
-                    <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
-                </div>
-                    <input placeholder="Texto da pergunta" type="text" minlength="20" required/>
-                    <input placeholder="Cor de fundo da pergunta" type="text" maxlength="7" minlength="7">
+            ${colocaPergunta()}
+
+            <!--ARRUMAR PARA ABRIR PERGUNTA CASO CLIQUE NO ICONE
+            <div class="card-edit">
+                <p class="title">Pergunta 2</p>
+                <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
             </div>
 
-            <!--<div class="pergunta-content">-->
-            <!--talvez faça sentido colocar pra esconder dps-->
-            <div class="group-inputs">
-                <p class="title">Resposta correta</p>
-                <input placeholder="Resposta correta" type="text" required/>
-                <input placeholder="URL da imagem" type="url">
-            </div>
+            <div class="card-edit">
+                <p class="title">Pergunta 3</p>
+                <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
+            </div>-->
+        `
+    }
+        //colocando final da pagina
+        document.querySelector(".cria-quiz").innerHTML += `
+            <button type="submit" class="btn-primario">Prosseguir para criar níves</button>
+        </form>`
 
-            <div class="group-inputs">
-                <p class="title">Respostas incorretas</p>
-                <input placeholder="Resposta incorreta 1" type="text" required/>
-                <input placeholder="URL da imagem 1" type="url">
-            </div>
-                
-            <div class="group-inputs">
-                <input placeholder="Resposta incorreta 2" type="text"> 
-                <input placeholder="URL da imagem 2" type="url">
-            </div>
+    
 
-            <div class="group-inputs">
-                <input placeholder="Resposta incorreta 3" type="text">
-                <input placeholder="URL da imagem 3" type="url">
-            </div>
-            <!-- fim de <div class="pergunta-content">-->
-
-            <!--fim do group-inputs-->
-        </div>
-
-
-        <!--ARRUMAR PARA ABRIR PERGUNTA CASO CLIQUE NO ICONE-->
-        <div class="card-edit">
-            <p class="title">Pergunta 2</p>
-            <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
-        </div>
-
-        <div class="card-edit">
-            <p class="title">Pergunta 3</p>
-            <ion-icon name="create-outline" data-identifier="expand"></ion-icon>
-        </div>
-
-        <button type="submit" class="btn-primario">Prosseguir para criar níves</button>
-
-    </form>
-    `
+    
 }
 
 
@@ -164,6 +265,8 @@ function setTelaQuatroCriaQuiz() {
     `
 }
 
+
+//SERVIDOR LAYOUT 3
 function deubom(requisicao){
     alert("deubom!");
     console.log(requisicao);
@@ -171,13 +274,14 @@ function deubom(requisicao){
 
 function post_api_criar_quizz(){
     const objeto = {
-        title: "Título do quizz",
-        image: "https://http.cat/411.jpg",
+        title: titulo,  
+        image: url_img_quizz,
         questions: [
             {
                 title: "Título da pergunta 1",
                 color: "#123456",
-                answers: [
+                answers:
+                [
                     {
                         text: "Texto da resposta 1",
                         image: "https://http.cat/411.jpg",
@@ -243,5 +347,5 @@ function post_api_criar_quizz(){
 
 }
 
-post_api_criar_quizz();
+//  post_api_criar_quizz();
 //FIM DO SCRIPT CRIA QUIZZ********************Pergunta 1
